ad34dde968bd1feb893c44429e757d7eb0fe3ac0
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-02-08 12:56:59 +0530

    Initial Commit

---
+++ src/test/java/com/dharmaraj/bookmyshow/BookmyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharmaraj.bookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
